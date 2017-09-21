fa6c3e62d0d7134ce70b8118e9f071bfd288d9b8
Adrian <adrianmeca@gmail.com>
2017-09-21 10:28:32 -0300

    cleanup

--- ejemplos/Ej09WebIntro/src/controlers/CtrlABMPersona.java
+++ ejemplos/Ej09WebIntro/src/controlers/CtrlABMPersona.java
@@ -12,65 +12,40 @@ public class CtrlABMPersona {
 	private DataPersona dataPer;
 	private DataCategoria dataCat;
 	
-	private ArrayList<Persona> pers;
-	
 	
 	public CtrlABMPersona(){
 		dataPer = new DataPersona();
 		dataCat = new DataCategoria();
-		pers= new ArrayList<Persona>();
-		pers.add(new Persona("12121212","Juan", "Perez",true));
-		pers.add(new Persona("13131313","Fulano", "De Tal",true));
 	}
 	
 	public void add(Persona p) throws Exception{
-		//this.pers.add(p);
 		dataPer.add(p);
 	}
 	
 	public void delete(Persona p)throws Exception{
-		//this.pers.remove(this.getByDni(p));
-		this.pers.remove(p);
+		dataPer.remove(p);
 	}
 	
 	public void update(Persona p)throws Exception{
-		this.delete(p);
-		this.add(p);
+		dataPer.update(p);
 	}
 	
 	public Persona getByDni(Persona p) throws Exception{
 		return this.dataPer.getByDni(p);
-		//return this.getByDni(p.getDni());
+		
 	}
 	
 	public Persona getByDni(String dni)throws Exception{
 		Persona p=new Persona();
 		p.setDni(dni);
 		return getByDni(p);
-		/*for (int i=0; i < this.pers.size(); i++){
-			if(pers.get(i).getDni().equals(dni)) {
-				return pers.get(i);
-				
-			}
-		}
-		return null; //ver error si falta
-		*/
 	}
 	
-	public Persona getByNombreApellido(Persona p){
-		
-		for (int i=0; i < this.pers.size(); i++){
-			if(pers.get(i).getNombre().equalsIgnoreCase(p.getNombre())
-				&& pers.get(i).getApellido().equalsIgnoreCase(p.getApellido())) {
-				return pers.get(i);		
-			}
-		}
-		return null; //ver error si falta
-		
+	public Persona getByNombreApellido(Persona p) throws Exception{
+		return dataPer.getByNombreApellido(p);		
 	}
 	
 	public ArrayList<Persona> getAll()throws Exception{
-		//return this.pers;
 		return dataPer.getAll();
 	}
 	
--- ejemplos/Ej09WebIntro/src/data/DataPersona.java
+++ ejemplos/Ej09WebIntro/src/data/DataPersona.java
@@ -161,4 +161,88 @@ public class DataPersona {
 		return p;
 	}
 
+	public void remove(Persona p) throws Exception{
+		PreparedStatement stmt=null;
+		try {
+			stmt=FactoryConexion.getInstancia().getConn()
+				.prepareStatement(
+					"delete from persona where id=?"
+				);
+			stmt.setInt(1, p.getId());
+			stmt.executeUpdate();
+		} catch (SQLException | AppDataException e) {
+			throw e;
+		}
+		try {
+			if(stmt!=null)stmt.close();
+			FactoryConexion.getInstancia().releaseConn();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public void update(Persona p) throws Exception{
+		PreparedStatement stmt=null;
+		try {
+			stmt=FactoryConexion.getInstancia().getConn()
+					.prepareStatement(
+					"update persona set dni=?, nombre=?, apellido=?, habilitado=?, categoriaId=? where id=?"
+					);
+			stmt.setString(1, p.getDni());
+			stmt.setString(2, p.getNombre());
+			stmt.setString(3, p.getApellido());
+			stmt.setBoolean(4, p.isHabilitado());
+			stmt.setInt(5, p.getCategoria().getId());
+			stmt.setInt(6, p.getId());
+			stmt.executeUpdate();
+		} catch (SQLException | AppDataException e) {
+			throw e;
+		}
+		try {
+			if(stmt!=null)stmt.close();
+			FactoryConexion.getInstancia().releaseConn();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+	public Persona getByNombreApellido(Persona per) throws Exception{
+		Persona p=null;
+		PreparedStatement stmt=null;
+		ResultSet rs=null;
+		try {
+			stmt=FactoryConexion.getInstancia().getConn().prepareStatement(
+					"select p.id, nombre, apellido, dni, habilitado, categoriaId, descripcion from persona p inner join categoria c on p.categoriaId=c.id where nombre=? and apellido=?");
+			stmt.setString(1, per.getNombre());
+			stmt.setString(2, per.getApellido());
+			rs=stmt.executeQuery();
+			if(rs!=null && rs.next()){
+					p=new Persona();
+					p.setCategoria(new Categoria());
+					p.setId(rs.getInt("id"));
+					p.setNombre(rs.getString("nombre"));
+					p.setApellido(rs.getString("apellido"));
+					p.setDni(rs.getString("dni"));
+					p.setHabilitado(rs.getBoolean("habilitado"));
+					p.getCategoria().setId(rs.getInt("categoriaId"));
+					p.getCategoria().setDescripcion(rs.getString("descripcion"));
+			}
+			
+		} catch (Exception e) {
+			throw e;
+		} finally{
+			try {
+				if(rs!=null)rs.close();
+				if(stmt!=null)stmt.close();
+				FactoryConexion.getInstancia().releaseConn();
+			} catch (SQLException e) {
+				throw e;
+			}
+		}
+
+		return p;
+	}
+
 }
